ce07e3426d99a9019b3003835d13869986fd66d6
arif93sulaiman <arifhip1993@yahoo.com>
2020-11-10 14:59:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
+import data from './data'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //changing string to int
+    let amount = parseInt(count)
+    if (count <= 1) {
+      amount = 1
+    }
+    if (count > 8) {
+      amount = 8
+    }
+    //selecting array from 0 to amount
+    setText(data.slice(0,amount))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraph
+        </label>
+        <input type="number" name="amount" id="amount" value={count} 
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">Generate</button>
+      </form>
+      <article className="lorem-text">
+       {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
   );
 }
 
